e19be2b83d4dfd0876f1b0d4989be1f66842195c
Devon Campbell <dec2180@columbia.edu>
2025-04-28 16:15:04 -0400

    fixed alist writer, some tentative results

--- lifted_hgp_4d.py
+++ lifted_hgp_4d.py
@@ -160,6 +160,44 @@ def split_cols(mat):
     mid = mat.shape[1] // 2
     return mat[:, :mid], mat[:, mid:]
 
+def save_alist(name, mat, j=None, k=None):
+    '''
+    Function converts parity check matrix into the format required for the RN decoder
+    '''
+    H=np.copy(mat)
+    H=H.T
+
+    if j is None:
+        j=int(max(H.sum(axis=0)))
+
+    if k is None:
+        k=int(max(H.sum(axis=1)))
+
+    m, n = H.shape # rows, cols
+    f = open(name, 'w')
+    print(n, m, file=f)
+    print(j, k, file=f)
+
+    for col in range(n):
+        print( int(H[:, col].sum()), end=" ", file=f)
+    print(file=f)
+    for row in range(m):
+        print( int(H[row, :].sum()), end=" ", file=f)
+    print(file=f)
+
+    for col in range(n):
+        for row in range(m):
+            if H[row, col]:
+                print( row+1, end=" ", file=f)
+        print(file=f)
+
+    for row in range(m):
+        for col in range(n):
+            if H[row, col]:
+                print(col+1, end=" ", file=f)
+        print(file=f)
+    f.close()
+
 class lifted_hgp_4d(css_code):
     """
     Extended lifted hypergraph product constructor that also
